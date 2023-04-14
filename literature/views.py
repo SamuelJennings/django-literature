b421b0c27ad54aabcf9570668ccf765b5be5aef7
Sam <samuel.scott.jennings@gmail.com>
2023-04-14 21:52:51 +0200

    add tox config to pyproject.toml

--- literature/views.py
+++ literature/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-from .models import Literature
-
-# Create your views here.
+from literature.models import Author, Literature
 
 
-def literature_list(request):
-    context = {
-        "qs": Literature.objects.all(),
-    }
-    return render(request, "literature/literature_list.html", context=context)
+class CitationMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["citation_style"] = self.citation_style
+        return context
+
+
+class LiteratureList(CitationMixin, ListView):
+    model = Literature
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related("authors")
+
+
+class LiteratureDetail(CitationMixin, DetailView):
+    model = Literature
+
+
+class AuthorList(ListView):
+    model = Author
+
+    def get_queryset(self):
+        return super().get_queryset().with_work_counts()
+
+
+class AuthorDetail(CitationMixin, DetailView):
+    model = Author
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["literature_list"] = context["author"].literature.prefetch_related("authors")
+        return context
