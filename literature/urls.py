28131780b02b65576edeffb7b985a40e495c7f57
Sam <samuel.scott.jennings@gmail.com>
2023-04-14 21:54:37 +0200

    add literature.urls

---
+++ literature/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "literature"
+urlpatterns = [
+    path("", views.LiteratureList.as_view(), name="list"),
+    path("authors/", views.AuthorList.as_view(), name="author_list"),
+    path("authors/<pk>/", views.AuthorDetail.as_view(), name="author_detail"),
+    path("<pk>/", views.LiteratureDetail.as_view(), name="detail"),
+]
