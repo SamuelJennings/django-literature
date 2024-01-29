16b4b0c814f239e338aed7268c2b5099b401b801
Sam Jennings <samuel.scott.jennings@gmail.com>
2024-01-29 13:47:59 +0000

    update views.py

--- literature/views.py
+++ literature/views.py
@@ -6,7 +6,15 @@ from django import forms
 from django.http import HttpResponse, JsonResponse
 from django.utils.encoding import force_str
 from django.views.generic import TemplateView
-from formset.views import FormCollectionView
+from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
+from formset.views import (
+    EditCollectionView,
+    FileUploadMixin,
+    FormCollectionView,
+    FormCollectionViewMixin,
+    IncompleteSelectResponseMixin,
+    SingleObjectMixin,
+)
 
 from literature.models import Literature
 
@@ -15,46 +23,37 @@ from .formset import LiteratureFormCollection
 from .tables import LiteratureTable
 
 
-class CitationMixin:
-    citation_style = "bootstrap"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["citation_style"] = self.citation_style
-        return context
-
-
-class LiteratureList(TemplateView, CitationMixin):
+class LiteratureList(TemplateView):
     table = LiteratureTable
-    template_name = "project/list.html"
+    template_name = "literature/literature_list.html"
 
 
-class LiteratureDetail(FormCollectionView, CitationMixin):
-    # model = SupplementaryMaterial
-    # form_class=LiteratureForm
+class LiteratureDetail(FormCollectionView):
     collection_class = LiteratureFormCollection
     extra_context = None
     template_name = "literature/literature_form.html"
 
-    # def get_object(self, queryset=None):
-    # if self.extra_context['add'] is False:
-    # return super().get_object(queryset)
-
-    def form_valid(self, form):
-        if (extra_data := self.get_extra_data()) and extra_data.get("delete") is True:
-            self.object.delete()
-            success_url = self.get_success_url()
-            response_data = {"success_url": force_str(success_url)} if success_url else {}
-            return JsonResponse(response_data)
-        return super().form_valid(form)
+    # def form_valid(self, form):
+    #     if (extra_data := self.get_extra_data()) and extra_data.get("delete") is True:
+    #         self.object.delete()
+    #         success_url = self.get_success_url()
+    #         response_data = {"success_url": force_str(success_url)} if success_url else {}
+    #         return JsonResponse(response_data)
+    #     return super().form_valid(form)
 
 
-class LiteratureForm(forms.ModelForm):
-    class Meta:
-        model = Literature
-        exclude = ["published", "year"]
+class LiteratureViewMixin(EditCollectionView):
+    model = Literature
+    collection_class = LiteratureFormCollection
+    template_name = "literature/literature_form.html"
 
-    # def __init__(self, data=None, *args, **kwargs):
-    #     # modify the incoming data to ensure that keys with hyphens are replaced with
-    #     data = {k.replace('-','_'):v for k,v in data.items()}
-    #     super().__init__(data, *args, **kwargs)
+
+# class LiteratureEditView(LiteratureViewMixin, TemplateResponseMixin, View):
+class LiteratureEditView(EditCollectionView):
+    model = Literature
+    collection_class = LiteratureFormCollection
+    template_name = "literature/literature_form.html"
+
+
+class LiteratureCreateView(FormCollectionView):
+    pass
