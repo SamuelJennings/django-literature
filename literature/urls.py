14baaad9f83249fde601075e695850112d7a9cbb
Sam Jennings <samuel.scott.jennings@gmail.com>
2024-01-29 13:47:47 +0000

    update urls.py

--- literature/urls.py
+++ literature/urls.py
@@ -6,4 +6,5 @@ app_name = "literature"
 urlpatterns = [
     path("", views.LiteratureList.as_view(), name="list"),
     path("<pk>/", views.LiteratureDetail.as_view(), name="detail"),
+    path("<pk>/edit/", views.LiteratureEditView.as_view(), name="edit"),
 ]
