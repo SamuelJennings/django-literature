bbef849a9fba5103832062ec199b551fb682e6b5
Sam <samuel.scott.jennings@gmail.com>
2024-10-08 09:02:54 +0200

    test app bug fixes

--- tests/urls.py
+++ tests/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("literature.urls")),
     path("select2/", include("django_select2.urls")),
     path("admin/", admin.site.urls),
-    path("", include("example.urls"))
+    path("", include("literature.urls")),
+    path("", include("example.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
