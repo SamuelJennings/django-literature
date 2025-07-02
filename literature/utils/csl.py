b41d6533d5008fc90d63183af5aa2319d17e1855
Sam <samuel.scott.jennings@gmail.com>
2025-07-02 16:48:24 +0200

    removed needless print statements

--- literature/utils/csl.py
+++ literature/utils/csl.py
@@ -5,11 +5,8 @@ from ..models import LiteratureItem
 
 
 def process_single_entry(entry: dict):
-    print(entry.get("citation-key"))
     instance = LiteratureItem.objects.filter(citation_key=entry.get("citation-key")).first()
     form = CSLForm(entry, instance=instance)
-    if entry.get("citation-key") == "Anderson_1940":
-        x = 1
     if form.is_valid():
         form.save()
     else:
